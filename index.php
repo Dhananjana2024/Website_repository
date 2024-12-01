0d2ec3b05e547c73db843d5260d218c5ba742398
Dhananjana2024 <Dhananjanabuddhi@gmail.com>
2024-12-01 12:34:47 +0530

    Copy the index.php
    copy the index.php file that i create  in to a website repostory

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width= , initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="CSS/index.css"> 
+</head>
+
+<body>
+
+  <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 ">
+    <div class="col-md-3 mb-2 mb-md-0">
+    <img src="resouses/Images/logo.png" width="100" height="50" role="img"  alt="logo" style="margin-left: 20px;">
+    </div>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 "> 
+      <li><a href="#" class="nav-link" style="color:white;" >Home</a></li>
+      <li><a href="News.php" class="nav-link" style="color:white;" >News</a></li>
+      <li><a href="Home.php" class="nav-link" style="color:white;" >Contact</a></li>
+      <li><a href="#" class="nav-link" style="color:white;" >Location</a></li>
+      <li><a href="#" class="nav-link" style="color:white;" >About us</a></li>
+    </ul>
+  </header>
+
+  <!-- Video Background -->
+  <video src="resouses/video/video.mp4" class="video-background" autoplay muted loop></video>
+  
+  <div class="content">
+    <h1>GLOBAL WEATHER iNfO</h1>
+    <p>This is my first page that i create for my weather web site </p>
+    <div>
+      <button type="button"><span></span>Login</button>
+      <button type="button"><span></span>Registration</button>
+    </div>
+  </div>
+  
+</body>
+</html>
+
+
+
+
+
